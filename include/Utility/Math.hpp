d640a9e4f74fba97fde64cacd7fd45835416a3ef
ThIsJaCk <y23657689@gmail.com>
2024-06-04 03:19:26 +0800

    create Math utilities function

---
+++ include/Utility/Math.hpp
@@ -0,0 +1,14 @@
+#ifndef MATH_HPP
+#define MATH_HPP
+
+#include <cmath>
+#include <limits>
+
+struct Math
+{
+    static bool IsZero(float value) {
+        return std::fabs(value) < std::numeric_limits<float>::epsilon();
+    }
+};
+
+#endif  // MATH_HPP
