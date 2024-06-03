5e4bc43fff5fffa9a052a8c75ea2dcee784b420b
ThIsJaCk <y23657689@gmail.com>
2024-06-04 03:20:04 +0800

    create UI data for loading

--- include/UIData.hpp
+++ include/UIData.hpp
@@ -1,12 +1,51 @@
 #ifndef UIDATA_HPP
 #define UIDATA_HPP
 
+#include <string>
+#include <vector>
 #include <glm/glm.hpp>
+
+namespace UI {
+    struct BaseNode {
+        std::string name;
+        glm::vec3 position = glm::vec3(0.0f);
+    };
+
+    struct Camera : public BaseNode {
+        glm::vec3 target = { 0.0, 0.0, -1.0 };
+        glm::vec3 up = { 0.0, 1.0, 0.0 };
+        float fovY = 45.0f;
+    };
+
+    struct Light : public BaseNode {
+
+    };
+
+    struct Project {
+        void AddCamera(Camera& camera) {
+            m_kCameras.push_back(camera);
+        }
+
+        std::vector< Camera > m_kCameras;
+        std::vector< Light > m_kLights;
+        std::vector< BaseNode > m_kObjects;
+    };
+};
 
 struct UIData {
     bool showUI = true;
     bool showDemoWindow = true;
     glm::vec4 clearColor = { 0.45f, 0.55f, 0.60f, 1.0f };
+
+    UI::Project project;
+    UIData() {
+        UI::Camera camera;
+        camera.name = "Default Camera";
+        camera.position = { 0.0, 0.0, 5.0 };
+        camera.target = { 0.0, 0.0, 4.0 };
+
+        project.AddCamera(camera);
+    }
 };
 
 #endif
